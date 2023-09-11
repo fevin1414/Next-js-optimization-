b95e9a401c4419a1f7f2d5967d58c016afe8b60b
fevin1414 <fevinbiju1414@gmail.com>
2023-09-11 15:00:39 -0500

    added dynamic title

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
-
+import Head from "next/head";
 import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
@@ -17,6 +17,10 @@ function AllEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>{events.title}</title>
+        <meta name="description" content="Learning optimization"/>
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
